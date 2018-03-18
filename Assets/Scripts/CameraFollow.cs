da61ceba72e9589a8f2372b616721f35cded8f93
lucklessfox <forick007@gmail.com>
2018-03-18 14:57:37 +0300

    hi

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+    public Vector3 offset;
+    public Vector3 viewOffset;
+    public float followSpeed;
+    public float turningSpeed;
+
+
+    void FixedUpdate ()
+    {
+        Vector3 offsetDifference = Quaternion.Inverse(target.rotation) * (transform.position - target.position) - offset;
+
+        float multiplier = offsetDifference.magnitude;
+
+        transform.position = Vector3.Lerp(transform.position, target.position + target.rotation * offset, followSpeed * Time.fixedDeltaTime * multiplier);
+        Vector3 directionToHead = (target.position + viewOffset) - transform.position;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(directionToHead), turningSpeed * Time.fixedDeltaTime);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+    public float speed = 5f;
+    public float turningSpeed = 5f;
+    private Rigidbody _rb;
+
+    public Rigidbody rb
+    {
+        get
+        {
+            if(_rb==null)
+            {
+                _rb = GetComponent<Rigidbody>();
+            }
+            return _rb;
+        }
+    }
+
+	void Start ()
+    {
+		
+	}
+	
+	void FixedUpdate ()
+    {
+        transform.Rotate(0f, Input.GetAxis("Horizontal") * turningSpeed, 0f);
+        rb.velocity = transform.rotation * new Vector3(0f, 0f, Input.GetAxis("Vertical")) * speed;
+		
+	}
+}
